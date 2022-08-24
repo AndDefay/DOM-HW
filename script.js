957c1ac43b397d295d1b29a8a5947a554c9e30a5
Andreaya Defay <andreayadefay@gmail.com>
2022-08-24 00:14:15 -0400

    done

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let quotes = [
 
 document.addEventListener("DOMContentLoaded", function(event) {
   // Random quote of the day generator
-  const randomQuote = function() {
+  const randomQuotel = function() {
     document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
   };
   randomQuote();
@@ -17,31 +17,55 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+  document.getElementById('main-title').innerHTML = "Dom's Page";
 
   // Part 2
-
+  document.querySelector('body')
+    .style.backgroundColor = 'green';
+    'green'
 
   // Part 3
-
+  var domList = document.getElementById("favorite-things");
+  var list = domList.getElementsByTagName("li")[5];
+  domList.removeChild(list);
 
   // Part 4
-
+  document.getElementById("special-title").style.fontSize = '2rem';
 
   // Part 5
-
-
+  var domRace = document.getElementById("past-races");
+  var race = document.getElementsByTagName("li")[8];
+  domRace.removeChild(race);
+ 
   // Part 6
-
+  var newCity = document.createElement("li");
+  var city = document.getElementById("past-races").getElementsByTagName("li")[2];
+  city.appendChild(newCity);
+  newCity.innerHTML = "San Francisco";
 
   // Part 7
-
+  var newBlog = document.createElement("div");
+  var blog = document.getElementsByClassName("main").getElementsByTagName("div");
+  blog.classList.add("blog-post purple");
+  var log = document.createElement("h1");
+  var og = document.createElement("p");
+  blog.appendChild("newBlog");
+  newBlog.appendChild("log");
+  newBlog.appendChild("og");
+  log.innerHTML = 'San Francisco';
+  og.innerHTML = 'FOR FAMILY';
 
   // Part 8
-
+  const randomQuote = function() {
+    document.querySelector('#quote-of-the-day').innerText = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
+  };
+  document.querySelector("quote-title").addEventListener("click", randomQuote);
 
   // Part 9
-
+  const mouseOut = document.getElementsByClassName("blog-post purple");
+  const mouseEnter = document.getElementsByClassName("blog-post purple");
+  mouseOut.toggleAttribute("purple");
+  mouseEnter.toggleAttribute("red");
 
 
 
